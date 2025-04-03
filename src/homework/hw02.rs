6437983cb671a9aef4c15894f46ac56a2b4c56e7
RymMyk <mykola.rymar@e-u.edu.ua>
2025-04-03 19:43:33 +0300

    Оновлено 3пракитичну

--- src/homework/hw02.rs
+++ src/homework/hw02.rs
@@ -1,21 +1,26 @@
-fn envelope() {
-    const W: u32 = 25;
-    const H: u32 = 20; 
+const W: u32 = 40;
+const H: u32 = 20;
 
-    for y in 0..H {
-        for x in 0..W {
-            let is_hor = y == 0 || y == H - 1;
-            let is_ver = x == 0 || x == W - 1;
-            let is_diag = x == y;
-            let is_diag2 = y == W - 1 - x;
-            let to_show = is_hor || is_ver || is_diag || is_diag2;
-            let sym = if to_show { '*' } else { ' ' };
-            print!("{} ", sym);
+fn envelope(width: u32, height: u32) {
+    let k: f32 = width as f32 / height as f32;
+    let mut output = String::new();
+
+    for y in 0..height {
+        for x in 0..width {
+            let is_hor = y == 0 || y == height - 1;
+            let is_ver = x == 0 || x == width - 1;
+            let is_diag = (x as f32 - (y as f32 * k)).abs() < 0.5;
+            let is_diag2 = (x as f32 - (width as f32 - 1.0 - (y as f32 * k))).abs() < 0.5;
+            let show = is_hor || is_ver || is_diag || is_diag2;
+
+            output.push(if show { '*' } else { ' ' });
         }
-        println!();
+        output.push('\n');
     }
+
+    print!("{}", output);
 }
 
 fn main() {
-    envelope();
+    envelope(W, H);
 }
